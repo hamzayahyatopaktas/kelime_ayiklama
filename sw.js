da3ce32b4c6c94325cd48084de46ba7d51f4c32c
Hamza Yahya Topaktaş <siirturkuniklimidir@gmail.com>
2026-04-18 18:53:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,70 @@
+// ⬇ Güncelleme yaparken bu versiyonu değiştirin
+const CACHE_VERSION = '2025-04-18-v1';
+const CACHE_NAME = 'kelime-ayiklama-' + CACHE_VERSION;
+const STATIC_ASSETS = [
+  '/kelime_ayiklama/manifest.json',
+  '/kelime_ayiklama/icon-192.png',
+  '/kelime_ayiklama/icon-512.png',
+  '/kelime_ayiklama/words.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      const oldCaches = keys.filter(k => k !== CACHE_NAME && k.startsWith('kelime-ayiklama-'));
+      const isUpdate = oldCaches.length > 0;
+
+      // Eski cache'leri sil
+      await Promise.all(oldCaches.map(k => caches.delete(k)));
+
+      // Önce tüm sekmeleri devral
+      await self.clients.claim();
+
+      // Güncelleme ise tüm açık sekmelere bildir
+      if (isUpdate) {
+        const clients = await self.clients.matchAll({ includeUncontrolled: true, type: 'window' });
+        clients.forEach(client => client.postMessage({ type: 'SW_UPDATED', version: CACHE_VERSION }));
+      }
+    })()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  const isHTML = url.pathname.endsWith('.html') || url.pathname.endsWith('/');
+
+  if (isHTML) {
+    event.respondWith(
+      fetch(event.request)
+        .then(res => {
+          caches.open(CACHE_NAME).then(c => c.put(event.request, res.clone()));
+          return res;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(cached =>
+        cached || fetch(event.request).then(res => {
+          caches.open(CACHE_NAME).then(c => c.put(event.request, res.clone()));
+          return res;
+        })
+      )
+    );
+  }
+});
+
+// Sürüm bilgisi talebi
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'GET_VERSION') {
+    event.ports[0].postMessage({ version: CACHE_VERSION });
+  }
+});
